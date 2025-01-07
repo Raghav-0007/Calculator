a86cfaa921cf33f6c608a102841c4523bad2a7c1
Raghav-0007 <rmyadav514@gmail.com>
2025-01-07 10:55:59 +0530

    addition

--- calculator.java
+++ calculator.java
@@ -1,5 +1,35 @@
-class calculator{
+import java.util.Scanner;
+
+public class calculator{
     public static void main(String[] args) {
-        System.out.println("Welcome to calculator");
+        Scanner sc= new Scanner(System.in);
+        while(true){
+        System.out.println("\nWelcome to calculator");
+        System.out.println("Enter '+' for addition");
+        System.out.println("Enter 'x' for Exit");
+        System.out.println("choose an operation: ");
+
+        char choice = sc.next().charAt(0);
+        if(choice == 'x' || choice == 'X') {
+            System.out.println("Exiting the calculator");
+            break;
+        }
+        System.out.println("Enter the first number: ");
+        double num1=sc.nextDouble();
+
+        System.out.println("Enter the second number: ");
+        double num2=sc.nextDouble();
+
+        switch (choice) {
+            case '+':
+                System.out.println("Result: " + (num1 + num2));
+                break;
+
+            default:
+            System.out.println("invalid choice");
+                break;
+        }
     }
+
+}
 }
