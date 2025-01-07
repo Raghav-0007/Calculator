df2b835218bedb579c335e0dfd97a13ff3d85a17
Raghav-0007 <rmyadav514@gmail.com>
2025-01-07 11:28:34 +0530

    subtraction

--- calculator.java
+++ calculator.java
@@ -1,35 +1,40 @@
 import java.util.Scanner;
 
-public class calculator{
+public class calculator {
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        while(true){
-        System.out.println("\nWelcome to calculator");
-        System.out.println("Enter '+' for addition");
-        System.out.println("Enter 'x' for Exit");
-        System.out.println("choose an operation: ");
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("\nWelcome to calculator");
+            System.out.println("Enter '+' for addition");
+            System.out.println("Enter '-' for subtraction");
+            System.out.println("Enter 'x' for Exit");
+            System.out.println("choose an operation: ");
 
-        char choice = sc.next().charAt(0);
-        if(choice == 'x' || choice == 'X') {
-            System.out.println("Exiting the calculator");
-            break;
-        }
-        System.out.println("Enter the first number: ");
-        double num1=sc.nextDouble();
-
-        System.out.println("Enter the second number: ");
-        double num2=sc.nextDouble();
-
-        switch (choice) {
-            case '+':
-                System.out.println("Result: " + (num1 + num2));
+            char choice = sc.next().charAt(0);
+            if (choice == 'x' || choice == 'X') {
+                System.out.println("Exiting the calculator");
                 break;
+            }
+            System.out.println("Enter the first number: ");
+            double num1 = sc.nextDouble();
 
-            default:
-            System.out.println("invalid choice");
-                break;
+            System.out.println("Enter the second number: ");
+            double num2 = sc.nextDouble();
+
+            switch (choice) {
+                case '+':
+                    System.out.println("Result: " + (num1 + num2));
+                    break;
+
+                case '-':
+                    System.out.println("Result: " + (num1 - num2));
+                    break;
+
+                default:
+                    System.out.println("invalid choice");
+                    break;
+            }
         }
+
     }
-
-}
 }
